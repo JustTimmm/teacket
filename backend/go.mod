9dc848c27a9a6cd23bd6f42e13cb886f9295b9d6
JustTimmm <francois09timeo@gmail.com>
2026-09-22 17:23:59 +0200

    better default server

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,13 @@
 module teacket
 
 go 1.26.8
+
+tool (
+	connectrpc.com/connect/cmd/protoc-gen-connect-go
+	google.golang.org/protobuf/cmd/protoc-gen-go
+)
+
+require (
+	connectrpc.com/connect v1.21.0 // indirect
+	google.golang.org/protobuf v1.36.12 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -6,8 +6,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Server starting on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
+	addr := "localhost:8080"
+	p := new(http.Protocols)
+	p.SetHTTP1(true)
+	p.SetUnencryptedHTTP2(true)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World!"))
+		if err != nil {
+			panic(fmt.Errorf("impossible to write the response %v\n", err))
+		}
+	})
+
+	s := &http.Server{
+		Addr:      addr,
+		Handler:   mux,
+		Protocols: p,
+	}
+
+	fmt.Printf("server started on %s\n", addr)
+	err := s.ListenAndServe()
+
+	if err != nil {
+		panic(fmt.Errorf("impossible to start the server %v\n", err))
 	}
 }
